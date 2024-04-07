6574c74c54cc8d44bd899600780258763a2aeb4b
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-07 05:02:57 +0300

    Add missing method

--- Modthara.Lari/Lsx/LsxDocument.cs
+++ Modthara.Lari/Lsx/LsxDocument.cs
@@ -8,4 +8,11 @@ public class LsxDocument
 {
     [XmlElement("version")] public required LariVersion Version { get; set; }
     [XmlElement("region")] public required List<LsxRegion> Regions { get; set; }
+
+    public static LsxDocument? FromStream(Stream stream)
+    {
+        var serializer = new XmlSerializer(typeof(LsxDocument));
+        var document = (LsxDocument?)serializer.Deserialize(stream);
+        return document;
+    }
 }
