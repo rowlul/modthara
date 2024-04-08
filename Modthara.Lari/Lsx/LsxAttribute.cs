0d678c0b9d9263443cb0d15f4c1493c3671d4889
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-08 19:31:59 +0300

    Change property order

--- Modthara.Lari/Lsx/LsxAttribute.cs
+++ Modthara.Lari/Lsx/LsxAttribute.cs
@@ -8,9 +8,9 @@ public class LsxAttribute
     [XmlAttribute("id")]
     public required string Id { get; set; }
 
-    [XmlAttribute("type")]
-    public required string Type { get; set; }
-
     [XmlAttribute("value")]
     public required string Value { get; set; }
+
+    [XmlAttribute("type")]
+    public required string Type { get; set; }
 }
