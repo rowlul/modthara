f721bb221ee8ada7c55bb12312d1f8c90c897371
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-15 20:51:33 +0300

    Add more views

---
+++ Modthara.App/Views/OverridesView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Modthara.App.Views;
+
+public partial class OverridesView : UserControl
+{
+    public OverridesView()
+    {
+        InitializeComponent();
+    }
+}
+
