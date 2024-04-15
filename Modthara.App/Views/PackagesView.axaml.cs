f0dfdf87f3360eb79ee353d0d452ba673d3d1527
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-15 20:43:24 +0300

    Add some views

---
+++ Modthara.App/Views/PackagesView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Modthara.App.Views;
+
+public partial class PackagesView : UserControl
+{
+    public PackagesView()
+    {
+        InitializeComponent();
+    }
+}
+
