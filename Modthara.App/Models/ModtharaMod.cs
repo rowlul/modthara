11f9162587bca2863f7cac3e013630603998c8d4
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-16 19:16:06 +0300

    Add StringifiedVersion property

--- Modthara.App/Models/ModtharaMod.cs
+++ Modthara.App/Models/ModtharaMod.cs
@@ -7,4 +7,6 @@ public class ModtharaMod : ModMetadata
     public required bool IsEnabled { get; set; }
     public required string FilePath { get; init; }
     public required ModFlags Flags { get; init; }
+
+    public string StringifiedVersion => Version.ToString();
 }
