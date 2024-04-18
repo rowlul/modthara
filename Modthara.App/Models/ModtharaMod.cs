c84b2b28835abdc37e892f2d717565918dde670f
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-18 16:01:03 +0300

    Implement LastModified property

--- Modthara.App/Models/ModtharaMod.cs
+++ Modthara.App/Models/ModtharaMod.cs
@@ -7,6 +7,7 @@ public class ModtharaMod : ModMetadata
     public required bool IsEnabled { get; set; }
     public required string FilePath { get; init; }
     public required ModFlags Flags { get; init; }
+    public DateOnly LastModified { get; set; } = DateOnly.FromDateTime(DateTime.Today);
 
     public string StringifiedVersion => Version.ToString();
 }
