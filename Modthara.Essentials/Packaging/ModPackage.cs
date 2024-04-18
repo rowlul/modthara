5c66aafe4dd7747c5be2077b927d1aa67d39259a
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-18 19:47:49 +0300

    Make IsEnabled non-required since it has a default value

--- Modthara.Essentials/Packaging/ModPackage.cs
+++ Modthara.Essentials/Packaging/ModPackage.cs
@@ -5,7 +5,7 @@ namespace Modthara.Essentials.Packaging;
 
 public class ModPackage : ModMetadata
 {
-    public required bool IsEnabled { get; set; }
+    public bool IsEnabled { get; set; }
     public required Package Package { get; init; }
     public required ModFlags Flags { get; init; }
     public DateOnly LastModified { get; set; } = DateOnly.FromDateTime(DateTime.Today);
