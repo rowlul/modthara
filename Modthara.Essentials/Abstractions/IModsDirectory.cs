15ef836000048c7e085480203fc983e66a980a27
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-23 23:55:39 +0300

    Improve docs

--- Modthara.Essentials/Abstractions/IModsDirectory.cs
+++ Modthara.Essentials/Abstractions/IModsDirectory.cs
@@ -16,8 +16,7 @@ public interface IModsDirectory
     /// Callback for each enumerated package.
     /// </param>
     /// <param name="onException">
-    /// Method that is executed whenever <see cref="IAsyncEnumerator{T}"/> in <see cref="ReadPackagesAsync"/> throws
-    /// an exception. If null, rethrows the exception.
+    /// Method to be executed whenever enumerator throws an exception. If null, rethrows the exception.
     /// </param>
     Task LoadPackagesAsync(Action<Exception>? onException = null, PackageReadCallback? asyncPackageCallback = null);
 
