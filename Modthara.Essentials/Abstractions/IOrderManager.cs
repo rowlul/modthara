be15530844bcc738fc8dab4c6d6d84c236c2eb0e
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-26 18:42:43 +0300

    Implement OrderManager

---
+++ Modthara.Essentials/Abstractions/IOrderManager.cs
@@ -0,0 +1,9 @@
+﻿using Modthara.Lari;
+
+namespace Modthara.Essentials.Abstractions;
+
+public interface IOrderManager
+{
+    Task<ModSettings> ReadOrderAsync(string path);
+    Task SaveOrderAsync(string path, ModSettings modSettings);
+}
---
+++ Modthara.Essentials/Packaging/OrderManager.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Abstractions;
+
+using Modthara.Essentials.Abstractions;
+using Modthara.Lari;
+using Modthara.Lari.Lsx;
+
+namespace Modthara.Essentials.Packaging;
+
+public class OrderManager : IOrderManager
+{
+    private readonly IFileSystem _fileSystem;
+
+    public OrderManager(IFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+
+    public async Task<ModSettings> ReadOrderAsync(string path)
+    {
+        await using var file = _fileSystem.FileStream.New(path, FileMode.Open, FileAccess.Read, FileShare.Read,
+            bufferSize: 4096, useAsync: true);
+
+        var lsx = LsxDocument.FromStream(file);
+        var order = new ModSettings(lsx);
+        await Task.Run(() => order.Sanitize()).ConfigureAwait(false);
+
+        return order;
+    }
+
+    public async Task SaveOrderAsync(string path, ModSettings modSettings)
+    {
+        await using var stream = modSettings.ToStream();
+        await using var file = _fileSystem.FileStream.New(path, FileMode.Create, FileAccess.Write, FileShare.None,
+            bufferSize: 4096, useAsync: true);
+        await stream.CopyToAsync(file).ConfigureAwait(false);
+    }
+}
