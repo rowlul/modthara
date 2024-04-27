875a82a985eef46ff8c06f6c0994ca844cdb64e0
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-27 18:18:35 +0300

    Create ModNotFoundException

---
+++ Modthara.Essentials/Packaging/ModNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using Modthara.Lari;
+
+namespace Modthara.Essentials.Packaging;
+
+public class ModNotFoundException(string? message = null, LariUuid? uuid = null, Exception? inner = null)
+    : Exception(message, inner)
+{
+    public LariUuid? Uuid { get; } = uuid;
+}
