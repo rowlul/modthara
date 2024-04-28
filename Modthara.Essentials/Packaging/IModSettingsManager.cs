170f09602acb395a5676d784c6e8e7f1cbfc19ed
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-28 13:02:20 +0300

    Implement managing json orders

--- Modthara.Essentials/Packaging/IModSettingsManager.cs
+++ Modthara.Essentials/Packaging/IModSettingsManager.cs
@@ -3,12 +3,12 @@
 namespace Modthara.Essentials.Packaging;
 
 /// <summary>
-/// Represents a service that handles mod orders.
+/// Manages mod settings.
 /// </summary>
 public interface IModSettingsManager
 {
     /// <summary>
-    /// Reads mod settings file.
+    /// Loads mod settings from file.
     /// </summary>
     /// <param name="path">
     /// Path to mod settings file.
@@ -16,13 +16,44 @@ public interface IModSettingsManager
     /// <returns>
     /// An instance of <see cref="ModSettings"/>.
     /// </returns>
-    Task<ModSettings> ReadModSettingsAsync(string path);
+    ValueTask<ModSettings> LoadModSettingsAsync(string path);
 
     /// <summary>
-    /// Saves mod settings to file.
+    /// Saves mod settings to LSX file.
     /// </summary>
-    /// <param name="path"></param>
-    /// <param name="modSettings"></param>
-    /// <returns></returns>
+    /// <param name="path">
+    /// Path to mod settings file.
+    /// </param>
+    /// <param name="modSettings">
+    /// Instance of <see cref="ModSettings"/>.
+    /// </param>
     Task SaveModSettingsAsync(string path, ModSettings modSettings);
+
+    /// <summary>
+    /// Parses JSON mod order to LSX mod settings.
+    /// </summary>
+    /// <param name="path">
+    /// Path to mod order file.
+    /// </param>
+    /// <param name="modPackages">
+    /// List of available mod packages.
+    /// </param>
+    /// <exception cref="ModNotFoundException">
+    /// Thrown if parsed mod is not found in <see cref="modPackages"/>.
+    /// </exception>
+    /// <returns>
+    /// Instance of <see cref="ModSettings"/>.
+    /// </returns>
+    ValueTask<ModSettings> LoadJsonOrderAsync(string path, IReadOnlyList<ModPackage> modPackages);
+
+    /// <summary>
+    /// Exports LSX mod settings to JSON mod order.
+    /// </summary>
+    /// <param name="path">
+    /// Path to mod order file.
+    /// </param>
+    /// <param name="modSettings">
+    /// Instance of <see cref="ModSettings"/>.
+    /// </param>
+    Task SaveJsonOrderAsync(string path, ModSettings modSettings);
 }
--- Modthara.Essentials/Packaging/ModSettingsManager.cs
+++ Modthara.Essentials/Packaging/ModSettingsManager.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO.Abstractions;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 using Modthara.Lari;
 using Modthara.Lari.Lsx;
@@ -16,12 +18,13 @@ public class ModSettingsManager : IModSettingsManager
     }
 
     /// <inheritdoc />
-    public async Task<ModSettings> ReadModSettingsAsync(string path)
+    public async ValueTask<ModSettings> LoadModSettingsAsync(string path)
     {
-        await using var file = _fileSystem.FileStream.New(path, FileMode.Open, FileAccess.Read, FileShare.Read,
+        var file = _fileSystem.FileStream.New(path, FileMode.Open, FileAccess.Read, FileShare.Read,
             bufferSize: 4096, useAsync: true);
+        var lsx = await Task.Run(() => LsxDocument.FromStream(file)).ConfigureAwait(false);
+        await file.DisposeAsync().ConfigureAwait(false);
 
-        var lsx = LsxDocument.FromStream(file);
         var order = new ModSettings(lsx);
         await Task.Run(() => order.Sanitize()).ConfigureAwait(false);
 
@@ -35,5 +38,58 @@ public class ModSettingsManager : IModSettingsManager
         await using var file = _fileSystem.FileStream.New(path, FileMode.Create, FileAccess.Write, FileShare.None,
             bufferSize: 4096, useAsync: true);
         await stream.CopyToAsync(file).ConfigureAwait(false);
+    }
+
+    /// <inheritdoc />
+    public async ValueTask<ModSettings> LoadJsonOrderAsync(string path, IReadOnlyList<ModPackage> modPackages)
+    {
+        await using var file = _fileSystem.FileStream.New(path, FileMode.Open, FileAccess.Read, FileShare.Read,
+            bufferSize: 4096, useAsync: true);
+
+        var document = await JsonDocument.ParseAsync(file).ConfigureAwait(false);
+
+        List<ModMetadata> mods = [];
+        foreach (var uuid in document.RootElement.GetProperty("Order").EnumerateArray()
+                     .Select(el => el.GetProperty("UUID").GetString()).OfType<string>())
+        {
+            ModPackage mod;
+
+            try
+            {
+                mod = modPackages.First(x => x.Uuid.Value == uuid);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ModNotFoundException($"Parsed mod not found: {uuid}.", new LariUuid(uuid), e);
+            }
+
+            mods.Add(mod);
+        }
+
+        var modSettings = new ModSettings(mods: mods);
+        return modSettings;
+    }
+
+    /// <inheritdoc />
+    public async Task SaveJsonOrderAsync(string path, ModSettings modSettings)
+    {
+        var order = new JsonArray();
+
+        foreach (var modMeta in modSettings.Mods)
+        {
+            var mod = new JsonObject { { "UUID", modMeta.Uuid.Value }, { "Name", modMeta.Name } };
+            order.Add(mod);
+        }
+
+        var root = new JsonObject { { "Order", order } };
+        await _fileSystem.File.WriteAllTextAsync(path,
+                root.ToJsonString(new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    PropertyNameCaseInsensitive = true,
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    AllowTrailingCommas = false
+                }))
+            .ConfigureAwait(false);
     }
 }
