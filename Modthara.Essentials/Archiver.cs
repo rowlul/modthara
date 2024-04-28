b79352f52c4103239ed9ee52576cd1727e290f78
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-28 14:37:54 +0300

    Implement opening zip files asynchronously

---
+++ Modthara.Essentials/Archiver.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Abstractions;
+using System.IO.Compression;
+
+namespace Modthara.Essentials;
+
+public class Archiver : IArchiver
+{
+    private readonly IFileSystem _fileSystem;
+
+    public Archiver(IFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+
+    public async Task<ZipArchive> OpenZipAsync(string path, ZipArchiveMode mode)
+    {
+        FileMode fileMode;
+        FileAccess fileAccess;
+        FileShare fileShare;
+
+        switch (mode)
+        {
+            case ZipArchiveMode.Read:
+                fileMode = FileMode.Open;
+                fileAccess = FileAccess.Read;
+                fileShare = FileShare.Read;
+                break;
+
+            case ZipArchiveMode.Create:
+                fileMode = FileMode.CreateNew;
+                fileAccess = FileAccess.Write;
+                fileShare = FileShare.None;
+                break;
+
+            case ZipArchiveMode.Update:
+                fileMode = FileMode.OpenOrCreate;
+                fileAccess = FileAccess.ReadWrite;
+                fileShare = FileShare.None;
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(mode));
+        }
+
+        var zipFileStream =
+            _fileSystem.FileStream.New(path, fileMode, fileAccess, fileShare, StreamBufferSize, useAsync: true);
+
+        var zipArchive = await Task.Run(() => new ZipArchive(zipFileStream, ZipArchiveMode.Read)).ConfigureAwait(false);
+        return zipArchive;
+    }
+
+    private const int StreamBufferSize = 0x1000;
+}
---
+++ Modthara.Essentials/IArchiver.cs
@@ -0,0 +1,8 @@
+﻿using System.IO.Compression;
+
+namespace Modthara.Essentials;
+
+public interface IArchiver
+{
+    Task<ZipArchive> OpenZipAsync(string path, ZipArchiveMode mode);
+}
