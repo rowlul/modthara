edec3b434b40e17a2099fff96f4241dd8cf015f2
rowlul <96617295+rowlul@users.noreply.github.com>
2024-04-29 19:23:06 +0300

    Make flags setter not init only

--- Modthara.Essentials/Packaging/ModPackage.cs
+++ Modthara.Essentials/Packaging/ModPackage.cs
@@ -5,10 +5,9 @@ namespace Modthara.Essentials.Packaging;
 
 public class ModPackage : ModMetadata
 {
-    public bool IsEnabled { get; set; }
     public required string Path { get; set; }
     public required Package Package { get; init; }
-    public required ModFlags Flags { get; init; }
+    public required ModFlags Flags { get; set; }
     public DateTime LastModified { get; set; } = DateTime.Now;
     public PackagedFile? ScriptExtenderConfig { get; set; }
 }
