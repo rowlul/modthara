ecac728458f2989ee24f4b1999927360ef87bcff
rowlul <96617295+rowlul@users.noreply.github.com>
2024-05-01 22:17:52 +0300

    Implement INotifyPropertyChanged

--- Modthara.Essentials/Packaging/ModPackage.cs
+++ Modthara.Essentials/Packaging/ModPackage.cs
@@ -1,13 +1,75 @@
-﻿using Modthara.Lari;
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+using Modthara.Lari;
 using Modthara.Lari.Pak;
 
 namespace Modthara.Essentials.Packaging;
 
-public class ModPackage : ModMetadata
+public class ModPackage : ModMetadata, INotifyPropertyChanged
 {
-    public required string Path { get; set; }
-    public required Package Package { get; init; }
-    public required ModFlags Flags { get; set; }
-    public DateTime LastModified { get; set; } = DateTime.Now;
-    public PackagedFile? ScriptExtenderConfig { get; set; }
+    private string _path;
+    private readonly Package _package;
+    private PackagedFile? _scriptExtenderConfig;
+    private ModFlags _flags;
+    private DateTime _lastModified = DateTime.Now;
+
+    public required string Path
+    {
+        get => _path;
+        [MemberNotNull(nameof(_path))]
+        set
+        {
+            _path = value;
+            OnPropertyChanged(nameof(Path));
+        }
+    }
+
+    public required Package Package
+    {
+        get => _package;
+        [MemberNotNull(nameof(_package))]
+        init
+        {
+            _package = value;
+            OnPropertyChanged(nameof(Package));
+        }
+    }
+
+    public PackagedFile? ScriptExtenderConfig
+    {
+        get => _scriptExtenderConfig;
+        set
+        {
+            _scriptExtenderConfig = value;
+            OnPropertyChanged(nameof(ScriptExtenderConfig));
+        }
+    }
+
+    public required ModFlags Flags
+    {
+        get => _flags;
+        set
+        {
+            _flags = value;
+            OnPropertyChanged(nameof(Flags));
+        }
+    }
+
+    public DateTime LastModified
+    {
+        get => _lastModified;
+        set
+        {
+            _lastModified = value;
+            OnPropertyChanged(nameof(LastModified));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
