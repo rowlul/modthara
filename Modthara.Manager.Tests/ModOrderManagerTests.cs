cb4e6c9d71a2e240e344a6ba4601f0fbe8f583b2
rowlul <96617295+rowlul@users.noreply.github.com>
2024-07-14 17:22:11 +0300

    Test LoadOrderAsync method

---
+++ Modthara.Manager.Tests/ModOrderManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Abstractions;
+
+#pragma warning disable CS1998
+
+namespace Modthara.Manager.Tests;
+
+public class ModOrderManagerTests
+{
+    private async static IAsyncEnumerable<ModOrderEntry> CreateModOrderEntries()
+    {
+        yield return new ModOrderEntry("1", "a");
+        yield return new ModOrderEntry("2", "b");
+        yield return new ModOrderEntry("3", "c");
+        yield return new ModOrderEntry("4", "d");
+    }
+
+    [Fact]
+    public async Task LoadOrderAsync_ReturnsModOrder()
+    {
+        var fs = new FileSystem();
+        var sut = new ModOrderManager(fs);
+
+        var expected = new ModOrder { Entries = CreateModOrderEntries() };
+        var actual = await sut.LoadOrderAsync("./TestFiles/test_order.json");
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
