fbc02172925ba34901bfc07f5580d69a08f24a8d
rowlul <96617295+rowlul@users.noreply.github.com>
2024-08-29 22:05:22 +0300

    Remove unused extension methods

--- Modthara.Lari/Extensions/LsxExtensions.cs
+++ Modthara.Lari/Extensions/LsxExtensions.cs
@@ -7,12 +7,6 @@ public static class LsxExtensions
     public static List<Module> ToShortDescModules(this IEnumerable<LsxNode> nodes) =>
         nodes.Where(x => x.Id == "ModuleShortDesc").Select(x => new Module(x)).ToList();
 
-    public static List<LsxNode> ToShortDescNodes(this IEnumerable<Module> modules) =>
-        modules.Select(x => x.ToNode()).ToList();
-
-    public static List<LsxNode> ToModuleNodes(this IEnumerable<ModuleBase> modules) =>
-        modules.Select(x => x.ToNode()).ToList();
-
     public static LariUuid GetUuid(this LsxNode node) => new(node.GetAttribute("UUID").Value);
 
     public static LariVersion GetVersion(this LsxNode node) =>
