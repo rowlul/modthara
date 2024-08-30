71ff9d5155d367738ef337310e5b5b5ccbbec259
rowlul <96617295+rowlul@users.noreply.github.com>
2024-08-31 00:58:02 +0300

    Remove UInt32 methods
    Not useful in BG3 anyways.

--- Modthara.Lari.Tests/LariVersionTests.cs
+++ Modthara.Lari.Tests/LariVersionTests.cs
@@ -6,15 +6,7 @@ public class LariVersionTests
     public void FromUInt64_ReturnsVersion()
     {
         var expected = new LariVersion { Major = 1, Minor = 0, Build = 0, Revision = 0 };
-        var actual = LariVersion.FromUInt64(36028797018963968UL);
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void FromUInt32_ReturnsVersion()
-    {
-        var expected = new LariVersion { Major = 1, Minor = 0, Build = 0, Revision = 0 };
-        var actual = LariVersion.FromUInt32(268435456);
+        var actual = new LariVersion(36028797018963968UL);
         actual.Should().Be(expected);
     }
 
@@ -22,15 +14,7 @@ public class LariVersionTests
     public void ToUInt64_ReturnsUInt64()
     {
         const ulong expected = 36028797018963968;
-        var actual = LariVersion.ToUInt64(new LariVersion { Major = 1, Minor = 0, Build = 0, Revision = 0 });
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void ToUInt32_ReturnsUInt32()
-    {
-        const int expected = 268435456;
-        var actual = LariVersion.ToUInt32(new LariVersion { Major = 1, Minor = 0, Build = 0, Revision = 0 });
+        var actual = new LariVersion { Major = 1, Minor = 0, Build = 0, Revision = 0 }.ToUInt64();
         actual.Should().Be(expected);
     }
 }
--- Modthara.Lari/LariVersion.cs
+++ Modthara.Lari/LariVersion.cs
@@ -24,12 +24,10 @@ public readonly struct LariVersion : IComparable<LariVersion>, IEquatable<LariVe
 
     public LariVersion(ulong version)
     {
-        this = FromUInt64(version);
-    }
-
-    public LariVersion(uint version)
-    {
-        this = FromUInt32(version);
+        Major = (uint)((version >> 55) & 0x7f);
+        Minor = (uint)((version >> 47) & 0xff);
+        Revision = (uint)((version >> 31) & 0xffff);
+        Build = (uint)(version & 0x7fffffff);
     }
 
     public LariVersion(uint major, uint minor, uint revision, uint build)
@@ -40,51 +38,11 @@ public readonly struct LariVersion : IComparable<LariVersion>, IEquatable<LariVe
         Build = build;
     }
 
-    public static LariVersion FromUInt64(ulong version) => new()
-    {
-        Major = (uint)((version >> 55) & 0x7f),
-        Minor = (uint)((version >> 47) & 0xff),
-        Revision = (uint)((version >> 31) & 0xffff),
-        Build = (uint)(version & 0x7fffffff),
-    };
-
-    public static LariVersion FromUInt32(uint version) => new()
-    {
-        Major = (version >> 28) & 0x0f,
-        Minor = (version >> 24) & 0x0f,
-        Revision = (version >> 16) & 0xff,
-        Build = version & 0xffff,
-    };
-
-    public static ulong ToUInt64(LariVersion version) =>
-        (ulong)(((long)version.Major & 0x7f) << 55 |
-                ((long)version.Minor & 0xff) << 47 |
-                ((long)version.Revision & 0xffff) << 31 |
-                ((long)version.Build & 0x7fffffff) << 0);
-
-    public ulong ToUInt64() => ToUInt64(this);
-
-    public static uint ToUInt32(LariVersion version) =>
-        (version.Major & 0x0f) << 28 |
-        (version.Minor & 0x0f) << 24 |
-        (version.Revision & 0xff) << 16 |
-        (version.Build & 0xffff) << 0;
-
-    public uint ToUInt32() => ToUInt32(this);
-
-    /// <inheritdoc />
-    public int CompareTo(LariVersion other)
-    {
-        if (Major != other.Major)
-            return Major.CompareTo(other.Major);
-        if (Minor != other.Minor)
-            return Minor.CompareTo(other.Minor);
-        if (Revision != other.Revision)
-            return Revision.CompareTo(other.Revision);
-        return Build.CompareTo(other.Build);
-    }
-
-    public override string ToString() => $"{Major}.{Minor}.{Revision}.{Build}";
+    public ulong ToUInt64() =>
+        (ulong)(((long)Major & 0x7f) << 55 |
+                ((long)Minor & 0xff) << 47 |
+                ((long)Revision & 0xffff) << 31 |
+                ((long)Build & 0x7fffffff) << 0);
 
     public static bool operator ==(LariVersion a, LariVersion b) => a.Major == b.Major &&
                                                                     a.Minor == b.Minor &&
@@ -100,8 +58,23 @@ public readonly struct LariVersion : IComparable<LariVersion>, IEquatable<LariVe
     public bool Equals(LariVersion other) => this == other;
 
     /// <inheritdoc />
+    public int CompareTo(LariVersion other)
+    {
+        if (Major != other.Major)
+            return Major.CompareTo(other.Major);
+        if (Minor != other.Minor)
+            return Minor.CompareTo(other.Minor);
+        if (Revision != other.Revision)
+            return Revision.CompareTo(other.Revision);
+        return Build.CompareTo(other.Build);
+    }
+
+    /// <inheritdoc />
     public override bool Equals(object? obj) => obj is LariVersion other && Equals(other);
 
     /// <inheritdoc />
     public override int GetHashCode() => HashCode.Combine(Major, Minor, Revision, Build);
+
+    /// <inheritdoc />
+    public override string ToString() => $"{Major}.{Minor}.{Revision}.{Build}";
 }
