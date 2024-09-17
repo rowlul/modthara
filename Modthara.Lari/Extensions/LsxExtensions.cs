7ca20a9b8223ffb918e6bf96aa597d57d8afb8b5
rowlul <96617295+rowlul@users.noreply.github.com>
2024-09-17 19:55:44 +0300

    Revert "Get only Version64"
    This reverts commit eef55f06

--- Modthara.Lari/Extensions/LsxExtensions.cs
+++ Modthara.Lari/Extensions/LsxExtensions.cs
@@ -4,13 +4,14 @@ namespace Modthara.Lari.Extensions;
 
 public static class LsxExtensions
 {
-    public static List<Module> ToShortDescModules(this List<LsxNode> nodes) =>
+    public static List<Module> ToShortDescModules(this IEnumerable<LsxNode> nodes) =>
         nodes.Where(x => x.Id == "ModuleShortDesc").Select(x => new Module(x)).ToList();
 
     public static LariUuid GetUuid(this LsxNode node) => new(node.GetAttribute("UUID").Value);
 
     public static LariVersion GetVersion(this LsxNode node) =>
-        new(Convert.ToUInt64((node.GetAttribute("Version64")).Value));
+        new(Convert.ToUInt64((node.GetAttributeOrDefault("Version64") ??
+                              node.GetAttribute("Version")).Value));
 
     public static int GetInt32(this LsxAttribute attribute) => Convert.ToInt32(attribute.Value);
 }
